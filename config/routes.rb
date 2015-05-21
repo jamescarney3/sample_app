0dab246dc7ac39db1b49b7a317b3da46987721df
James Carney <jmcarneyiii@gmail.com>
2015-05-21 00:54:26 +0000

    Add password resets and email configuration

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   delete 'logout'  => 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
